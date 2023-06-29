c9c6369f97ff54f956b147c5a6d0f3f8bd1ea049
purple-sunflower <jgh0711@naver.com>
2023-06-29 21:47:36 +0900

    jjang

--- src/component/playMusic.js
+++ src/component/playMusic.js
@@ -5,11 +5,20 @@ import { faBackward } from "@fortawesome/free-solid-svg-icons";
 import { faForward } from "@fortawesome/free-solid-svg-icons";
 
 import '../css/playMusic.css'
-import { useState } from "react";
 import Header from './header';
 import Spicy from '../images/albumCover/spicy.jpg'
+import { useState } from "react";
 
-function playMusic() {
+function PlayMusic() {
+
+    const clickBtn = () => {
+        alert("You click your button!");
+    } // 버튼 작동 확인   
+
+    const [isPlay, setPlay] = useState(true);
+    const toggleBtn = () => {
+        setPlay(!isPlay); // playBtn
+    }
 
     return(
         <div id='playMusic-wrap'>
@@ -19,13 +28,15 @@ function playMusic() {
                 <span className="content" id ='title'>제목: Spicy </span>
                 <span className="content" id ='name'>가수: aespa</span> 
                 <span className="content" id='btnCollection'>
-                    <FontAwesomeIcon icon ={faBackward}/>
-                    <FontAwesomeIcon icon ={faPlay}/>
-                    <FontAwesomeIcon icon ={faForward}/>
+                    <FontAwesomeIcon icon ={faBackward} onClick={clickBtn}/>
+                    <div onClick={toggleBtn}>
+                        {isPlay ? <FontAwesomeIcon icon ={faPlay}/> : <FontAwesomeIcon icon ={faPause}/>}
+                    </div>
+                    <FontAwesomeIcon icon ={faForward} onClick={clickBtn}/>
                 </span>
             </div>
         </div>
     );
 }
 
-export default playMusic;
+export default PlayMusic;
