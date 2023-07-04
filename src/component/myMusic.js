d7e262325a5266aed417c2a78d7c8cae7f94b4a8
purple-sunflower <jgh0711@naver.com>
2023-07-04 14:43:30 +0900

    jjang

--- src/component/myMusic.js
+++ src/component/myMusic.js
@@ -1,11 +1,24 @@
 import '../css/myMusic.css'
 import Header from './header';
+import User from '../images/user.png'
 
 function MyMusic() {
     return(
         <div id='myMusic-wrap'>
             <Header/>
             내 보관함 입니다.
+            <div id='user'>
+                <div id='user-left'>
+                    <div id='user-img'>
+                        <img src={User}/>
+                    </div>
+                </div>
+                <div id='user-right'>
+                    <div id="user-name">
+                        사용자 이름
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
