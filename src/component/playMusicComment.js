eb018a54defb2ca7990db723850c4cd4df642f5d
purple-sunflower <jgh0711@naver.com>
2023-07-20 17:17:50 +0900

    jjang

--- src/component/playMusicComment.js
+++ src/component/playMusicComment.js
@@ -8,15 +8,15 @@ function PlayMusicComment() {
         <div id="commentRead">
           전체 코멘트 볼 수 있즘.
           <CommentBox nickname ="카리나는 신" comment="유지민 사랑해"/>
+          <CommentBox nickname ="마크완댜님" comment="타 팬인데 에스파 노래 반박불가.. 넘 좋음"/>
           <CommentBox nickname ="지제리" comment="랩 쫀득한거 보소"/>
+          <CommentBox nickname ="고추가루를 날려" comment="어머 내 주제곡인데?"/>
+          <CommentBox nickname ="난항상웃쟈니" comment="우리 오빠랑 콜라보 해주세요 ㅠㅠ"/>
           <CommentBox nickname ="너와나의윈터" comment="겨울이 고음 대박"/>
+          <CommentBox nickname ="스렌펠" comment="Oh.. This song is my favorite song!"/>
           <CommentBox nickname ="아임닝닝스팬" comment="닝닝이 목소리 존예"/>
           <CommentBox nickname ="블랙맘바" comment="내가 낄 곳이 없네"/>
-          <CommentBox nickname ="카리나는 신" comment="유지민 사랑해"/>
-          <CommentBox nickname ="지제리" comment="랩 쫀득한거 보소"/>
-          <CommentBox nickname ="너와나의윈터" comment="겨울이 고음 대박"/>
-          <CommentBox nickname ="아임닝닝스팬" comment="닝닝이 목소리 존예"/>
-          <CommentBox nickname ="블랙맘바" comment="내가 낄 곳이 없네"/>
+          <CommentBox nickname ="암어귄카" comment="에스파 신곡 나왔다면서요?"/>
         </div>
         <div id="commentInput">
           <input placeholder="당신의 생각을 적어보세요:)"/>
--- src/component/recMusicBox.js
+++ src/component/recMusicBox.js
@@ -9,11 +9,36 @@ import React from 'react';
 import '../css/recMusicBox.css'
 
 function RecMusicBox(props){
+    
+    const mouseOnPlayBtn = () => {
+        document.getElementById("play").style= "display:block;";
+    } 
+
+    const mouseOutPlayBtn = () => {
+        document.getElementById("play").style= "display:none;";
+    } 
+
+    // 230720 호버 시, 플레이 버튼 나오게
+    // 두가지 문제
+    // 1. 왜 다른 요소에 호버해도, 첫번째 요소에만 재생 버튼이 나오는가?
+    // position의 absolute를 사용해서?
+    // 2. 재생 버튼과 배경이 함께 투명해지는가? (배경만 투명하게 하고픔)
+    
+    // 1번 해결:
+    // 2번 해결(230720) > play 요소를 탑과 같은 레벨에. 
+    // 추가적 문제 ~ #recMusicBox-wrap:hover로 하면 동일 문제 발생
+    // 즉, 전체 요소의 배경만 투명해지게 하진 못함.. (난 이걸 원하는디..)
+
+
+
     return(
-        <div id='recMusicBox-wrap'>
+        <div id='recMusicBox-wrap'onMouseOver={mouseOnPlayBtn} onMouseOut={mouseOutPlayBtn}>
          <div id='top'>
             {props.imgSrc} 
          </div>
+         <div id='play'>
+                ▶
+        </div>
          <div id='bottom'>
             <span>{props.title}{/* 제목 */}</span>
             <span>{props.name}{/* 가수 */}</span>
